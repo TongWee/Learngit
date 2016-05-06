633c17f91c6adadb1bce9cbe299e904e3dc061dd
TongWee <308992640@qq.com>
2016-05-06 21:39:19 +0800

    add

--- mergeArray.cpp
+++ mergeArray.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+//Merge two vector
 using namespace std;
 
-void merge(vector<int> a, int m, vector<int> b, int n){
-    int k = m + n - 1;
-    a[k--] = b[0];
+vector<int> merge(vector<int> a, vector<int> b){
+    int len_a = a.size();
+    int len_b = b.size();
+    vector<int> res;
+    vector<int> :: iterator itra = a.begin();
+    vector<int> :: iterator itrb = b.begin();
+    while(itra!=a.end() && itrb!=b.end()){
+        if(*itra < *itrb)
+            res.push_back(*itra++);
+        else
+            res.push_back(*itrb++);
+    }
+    while(itra!=a.end())
+        res.push_back(*itra++);
+    while(itrb!=b.end())
+        res.push_back(*itrb++);
+    return res;   
 }
 
 int main(int argc, const char *argv[])
 {
-    vector<int> a;
-    vector<int> b;
-    a.push_back(1);
-    a.push_back(2);
-    b.push_back(3);
-    merge(a, 2, b, 1);
+    int src[10] = {1,2,3,4,5,6,7,8,9,10};
+    vector<int> a(src, src+10);
+    vector<int> b(src, src+10);
+    vector<int> res = merge(a, b);
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
